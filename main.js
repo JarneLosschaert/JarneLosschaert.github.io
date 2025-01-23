3d4cb49f0a98cb2d6071e85fe2199b5f8d0bb65b
Jarne Losschaert <jarnelosschaert@gmail.com>
2025-01-23 16:23:31 +0100

    Updates

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import { MotionPlugin } from '@vueuse/motion'
-import App from './App.vue'
-import Portfolio from './views/Portfolio.vue'
-import Contact from './components/Contact.vue'
-import NotFound from './views/NotFound.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { MotionPlugin } from "@vueuse/motion";
+import Portfolio from "../src/views/Portfolio.vue";
+import Contact from "../src/views/Contact.vue";
+import NotFound from "../src/views/NotFound.vue";
+import App from "./App.vue";
+
 
 const router = createRouter({
     history: createWebHistory(),
